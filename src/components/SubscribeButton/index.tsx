efc7bdeb2fb67f6705cb63d845a92372039d11d8
wesleygomess <wesley.micro1@hotmail.com>
2022-03-24 01:01:44 -0300

    (rota de checkout)

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,21 @@
 import styles from './styles.module.scss';
-
+import { useSession, signIn } from 'next-auth/react';
 interface SubscribeButtonProps {
   priceId: string
 }
 
 export function SubscribeButton({priceId} : SubscribeButtonProps){
+  const {data: session} = useSession();
+
+  function handleSubscribe() {
+    if(!session){
+      signIn("github");
+      return;
+    }
+
+
+  }
+
   return(
     <button 
       type="button"
