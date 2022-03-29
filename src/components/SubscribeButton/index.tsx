02b1a10030f52211f64a03c24b1f26c55af01743
wesleygomess <wesley.micro1@hotmail.com>
2022-03-28 23:51:14 -0300

    (subscribe redirecionado)

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.scss';
 import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 interface SubscribeButtonProps {
   priceId: string
 }
@@ -7,18 +9,30 @@ interface SubscribeButtonProps {
 export function SubscribeButton({priceId} : SubscribeButtonProps){
   const {data: session} = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if(!session){
       signIn("github");
       return;
     }
 
+    try {
+      const reponse = await api.post("/subscribe");
 
+      const {sessionId} = reponse.data;
+
+      const stripe = getStripeJs();
+
+      (await stripe).redirectToCheckout({ sessionId });      
+
+    } catch(error) {
+      console.log("ERRO API STRIPE..", error)
+    }
   }
 
   return(
     <button 
       type="button"
+      onClick={handleSubscribe}
       className={styles.subscribeButton}
     >
       Subscribe now 
